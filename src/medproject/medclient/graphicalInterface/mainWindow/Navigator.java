1276a57d06388b239f7c2d8789bccbf7072104d0
Vlad Sandu <vladsandu1994@gmail.com>
2015-11-24 18:17:16 +0200

    Add getPaneFromScene method

--- src/medproject/medclient/graphicalInterface/mainWindow/Navigator.java
+++ src/medproject/medclient/graphicalInterface/mainWindow/Navigator.java
@@ -15,32 +15,68 @@ public class Navigator {
 
 	public static final String LOGIN_SCENE = "./loginScene/loginScene.fxml";
 	public static final String LOADING_SCENE = "./loadingScene/loadingScene.fxml";
+	public static final String MAIN_SCENE = "./mainScene/mainScene.fxml";
+	public static final String PERSON_TAB_SCENE = "./personTabScene/personTabScene.fxml";
 
 	private static MainWindow mainWindow = null;
-	
+	private static String currentScene = "";
+	private static ControllerInterface currentController = null;
+
 	public static void setMainWindow(MainWindow mainWindow) {
 		Navigator.mainWindow = mainWindow;
 	}
 
-	public static void loadScene(String fxml) {
-
+	public static void loadScene(final String fxml) {
 		if(mainWindow == null){
 			LOG.severe("Navigator not initialized");
 			return;
 		}
 
+		mainWindow.runAndWait(new Runnable(){
+
+			@Override
+			public void run() {
+				try {
+					FXMLLoader loader = new FXMLLoader(Navigator.class.getResource(fxml));
+					Pane pane = (Pane) loader.load();
+					Scene scene = new Scene(pane);
+
+					ControllerInterface currentController = (ControllerInterface)loader.getController();
+					currentController.init(mainWindow.getDataLoader(), mainWindow.getUpdateExecutor());
+					
+					mainWindow.setScene(scene);
+					mainWindow.getPrimaryWindow().sizeToScene();
+					
+					Navigator.currentController = currentController;
+					currentScene = fxml;
+				} catch (IOException e) {
+					LOG.severe("Cannot find fxml file location for value: " + fxml);
+				}				
+			}
+
+		});
+	}
+
+	public static Pane getPaneFromScene(final String fxml){
+		FXMLLoader loader = new FXMLLoader(Navigator.class.getResource(fxml));
+		Pane pane = null;
 		try {
-			FXMLLoader loader = new FXMLLoader(Navigator.class.getResource(fxml));
-			Pane pane = (Pane) loader.load();
-			Scene scene = new Scene(pane);
-			
+			pane = (Pane) loader.load();
 			ControllerInterface currentController = (ControllerInterface)loader.getController();
 			currentController.init(mainWindow.getDataLoader(), mainWindow.getUpdateExecutor());
-
-			mainWindow.setScene(scene);
 		} catch (IOException e) {
-			LOG.severe("Cannot find fxml file location for value: " + fxml);
+			LOG.severe("Couldn't load pane from: " + fxml + "; " + e.getMessage());
 		}
+		
+		return pane;
+	}
+		
+	public static String getCurrentScene() {
+		return currentScene;
+	}
+
+	public static ControllerInterface getCurrentController() {
+		return currentController;
 	}
 
 }
