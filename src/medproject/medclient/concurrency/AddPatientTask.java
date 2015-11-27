8cfd07ec4e2b3cf7821e5428cdcf9484ac819659
Vlad Sandu <vladsandu1994@gmail.com>
2015-11-27 12:24:50 +0200

    Create AddPatientTask

---
+++ src/medproject/medclient/concurrency/AddPatientTask.java
@@ -0,0 +1,44 @@
+package medproject.medclient.concurrency;
+
+import medproject.medclient.dataLoader.DataLoader;
+import medproject.medclient.graphicalInterface.addPersonWindow.AddPersonController;
+import medproject.medclient.utils.GUIUtils;
+import medproject.medlibrary.concurrency.CustomTask;
+import medproject.medlibrary.concurrency.RequestCodes;
+import medproject.medlibrary.patient.Patient;
+
+public class AddPatientTask extends CustomTask{
+
+	private final DataLoader dataLoader;
+	private final AddPersonController controller;
+	
+	public AddPatientTask(DataLoader dataLoader, AddPersonController controller) {
+		super(RequestCodes.ADD_PATIENT_REQUEST);
+		this.dataLoader = dataLoader;
+		this.controller = controller;
+	}
+
+	@Override
+	public void run() {
+		try {
+			getLatch().await();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		if(data != null){
+			try{
+				Patient patient = (Patient) data;
+				dataLoader.addPatient(patient);
+			}catch(ClassCastException e){
+				GUIUtils.showErrorDialog("Add Patient Error", "Data corrupted");
+			}
+			
+			controller.closeWindow();
+		}
+		else{
+			controller.resetInterface();
+		}
+	}
+
+}
