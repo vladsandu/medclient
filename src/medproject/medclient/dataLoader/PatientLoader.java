07ca9f4ea67642a4db54ce3e41c46a994daf0951
Vlad Sandu <vladsandu1994@gmail.com>
2015-11-30 15:37:43 +0200

    Refactor PatientLoader

--- src/medproject/medclient/dataLoader/PatientLoader.java
+++ src/medproject/medclient/dataLoader/PatientLoader.java
@@ -25,69 +25,26 @@ public class PatientLoader {
 			processPatientListRequest(request);
 			break;
 		case RequestCodes.PATIENT_RECORD_BY_CNP_REQUEST:
-			processPatientRecordByCNPRequest(request);
+			processGUITaskRequest(request);
 			break;
 		case RequestCodes.ADD_PATIENT_REQUEST:
-			processAddPatientRequest(request);
+			processGUITaskRequest(request);
 			break;
 		case RequestCodes.UPDATE_PATIENT_ADDRESS_REQUEST:
-			processUpdatePatientAddressRequest(request);
+			processGUITaskRequest(request);
 			break;
 		case RequestCodes.DELETE_PATIENT_REQUEST:
-			processDeletePatientRequest(request);
+			processGUITaskRequest(request);
 			break;
 		case RequestCodes.UNREGISTER_PATIENT_REQUEST:
-			processUnregisterPatientRequest(request);
+			processGUITaskRequest(request);
 			break;
 		case RequestCodes.REGISTER_PATIENT_REQUEST:
-			processRegisterPatientRequest(request);
+			processGUITaskRequest(request);
 			break;
 		}
 	}
 
-	//AddPatientRequest
-	public void makeAddPatientRequest(int PID, int PIN){
-		dataLoader.makeRequest(new Request(RequestCodes.ADD_PATIENT_REQUEST, PID, PIN));
-	}
-
-	private void processAddPatientRequest(Request request){
-		LOG.info(request.getMessage());
-		if(request.getStatus() == RequestStatus.REQUEST_COMPLETED){
-		}
-		else{
-			GUIUtils.showErrorDialog("Add Patient Error", request.getMessage());
-		}
-		dataLoader.processGuiTask(request);
-	}
-	//UpdatePatientRecord
-	public void makeUpdatePatientAddressRequest(Address address){
-		dataLoader.makeRequest(new Request(RequestCodes.UPDATE_PATIENT_ADDRESS_REQUEST, address));
-	}
-
-	private void processUpdatePatientAddressRequest(Request request){
-		LOG.info(request.getMessage());
-
-		if(request.getStatus() != RequestStatus.REQUEST_COMPLETED){
-			GUIUtils.showErrorDialog("Patient Address Update Request Error", request.getMessage());
-		}
-
-		dataLoader.processGuiTask(request);
-	}
-	//GetPatientRecordByCNP
-	public void makePatientRecordByCNPRequest(String cnp){
-		dataLoader.makeRequest(new Request(RequestCodes.PATIENT_RECORD_BY_CNP_REQUEST, cnp));
-	}
-
-	private void processPatientRecordByCNPRequest(Request request){
-		LOG.info(request.getMessage());
-
-		if(request.getStatus() != RequestStatus.REQUEST_COMPLETED){
-			GUIUtils.showErrorDialog("Patient Record Request Error", request.getMessage());
-		}
-
-		dataLoader.processGuiTask(request);
-	}
-	//PatientList
 	public void loadPatientList(){
 		dataLoader.makeRequest(new Request(RequestCodes.PATIENT_LIST_REQUEST, null));
 	}
@@ -109,52 +66,40 @@ public class PatientLoader {
 			//fatal
 		}
 	}
-	//DeletePatient
 
-	public void makeDeletePatientRequest(int patientID) {
-		dataLoader.makeRequest(new Request(RequestCodes.DELETE_PATIENT_REQUEST, patientID));
-	}
-
-	private void processDeletePatientRequest(Request request) {
+	private void processGUITaskRequest(Request request) {
 		LOG.info(request.getMessage());
 
 		if(request.getStatus() != RequestStatus.REQUEST_COMPLETED){
-			GUIUtils.showErrorDialog("Couldn't delete patient", request.getMessage());
+			
+			GUIUtils.showErrorDialog("Error", request.getMessage());
 		}
 
 		dataLoader.processGuiTask(request);
 	}
-	//UnregisterPatient
+//TODO: maybe refactor
+	public void makeAddPatientRequest(int PID, int PIN){
+		dataLoader.makeRequest(new Request(RequestCodes.ADD_PATIENT_REQUEST, PID, PIN));
+	}
+
+	public void makeUpdatePatientAddressRequest(Address address){
+		dataLoader.makeRequest(new Request(RequestCodes.UPDATE_PATIENT_ADDRESS_REQUEST, address));
+	}
+
+	public void makePatientRecordByCNPRequest(String cnp){
+		dataLoader.makeRequest(new Request(RequestCodes.PATIENT_RECORD_BY_CNP_REQUEST, cnp));
+	}
+	
+	public void makeDeletePatientRequest(int patientID) {
+		dataLoader.makeRequest(new Request(RequestCodes.DELETE_PATIENT_REQUEST, patientID));
+	}
 
 	public void makeUnregisterPatientRequest(int patientID) {
 		dataLoader.makeRequest(new Request(RequestCodes.UNREGISTER_PATIENT_REQUEST, patientID));
 
 	}
 
-	private void processUnregisterPatientRequest(Request request) {
-		LOG.info(request.getMessage());
-
-		if(request.getStatus() != RequestStatus.REQUEST_COMPLETED){
-			GUIUtils.showErrorDialog("Couldn't unregister patient", request.getMessage());
-		}
-
-		dataLoader.processGuiTask(request);
-	}
-
-	//RegisterPatient
-
 	public void makeRegisterPatientRequest(int patientID) {
 		dataLoader.makeRequest(new Request(RequestCodes.REGISTER_PATIENT_REQUEST, patientID));
-
-	}
-
-	private void processRegisterPatientRequest(Request request) {
-		LOG.info(request.getMessage());
-
-		if(request.getStatus() != RequestStatus.REQUEST_COMPLETED){
-			GUIUtils.showErrorDialog("Couldn't register patient", request.getMessage());
-		}
-
-		dataLoader.processGuiTask(request);
 	}
 }
