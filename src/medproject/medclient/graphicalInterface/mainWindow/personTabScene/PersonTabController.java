8e94ecdc6c7d4a193c90887996ff363282dcbd83
Vlad Sandu <vladsandu1994@gmail.com>
2015-12-05 15:04:04 +0200

    Refactor the TableView to use the PatientTableView

--- src/medproject/medclient/graphicalInterface/mainWindow/personTabScene/PersonTabController.java
+++ src/medproject/medclient/graphicalInterface/mainWindow/personTabScene/PersonTabController.java
@@ -2,7 +2,6 @@ package medproject.medclient.graphicalInterface.mainWindow.personTabScene;
 
 import java.util.function.Predicate;
 
-import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -13,18 +12,17 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 import medproject.medclient.dataLoader.DataLoader;
 import medproject.medclient.graphicalInterface.ControllerInterface;
 import medproject.medclient.graphicalInterface.addPersonWindow.AddPersonWindow;
 import medproject.medclient.graphicalInterface.patientDataWindow.PatientDataWindow;
+import medproject.medlibrary.graphics.structures.PatientTableView;
 import medproject.medlibrary.patient.Patient;
 
 public class PersonTabController implements ControllerInterface{
@@ -32,20 +30,9 @@ public class PersonTabController implements ControllerInterface{
 	private DataLoader dataLoader;
 
 	//TODO: Customize cell style according to the patient.
-	@FXML TableView<Patient> patientTable;
-	@FXML TableColumn<Patient, String> cnpColumn;
-	@FXML TableColumn<Patient, String> firstNameColumn;
-	@FXML TableColumn<Patient, String> lastNameColumn;
-	@FXML TableColumn<Patient, String> genderColumn;
-	@FXML TableColumn<Patient, String> categoryColumn;
-	@FXML TableColumn<Patient, String> birthDateColumn;
-	@FXML TableColumn<Patient, String> deceaseDateColumn;
-	@FXML TableColumn<Patient, String> registrationDateColumn;
-	@FXML TableColumn<Patient, String> unregistrationDateColumn;
-	@FXML TableColumn<Patient, String> countyColumn;
-	@FXML TableColumn<Patient, String> cityColumn;
-	@FXML TableColumn<Patient, String> streetColumn;
-
+	TableView<Patient> patientTable;
+	
+	@FXML ScrollPane tableScrollPane;
 	@FXML RadioButton allInsuredPatientRadio;
 	@FXML RadioButton insuredPatientRadio;
 	@FXML RadioButton uninsuredPatientRadio;
@@ -70,10 +57,8 @@ public class PersonTabController implements ControllerInterface{
 	@Override
 	public void init(final DataLoader dataLoader, Stage stage) {
 		this.dataLoader = dataLoader;
-
-		patientTable.setEditable(false);
-		
-		setColumnValues();
+		patientTable = new PatientTableView();
+		tableScrollPane.setContent(patientTable);
 		setTableFilters();
 		setListeners();
 	}
@@ -335,80 +320,5 @@ public class PersonTabController implements ControllerInterface{
 		unregisteredPatientRadio.setToggleGroup(registeredRadioGroup);
 		unregisteredPatientRadio.setUserData("unregisteredPatient");
 		
-	}
-
-	private void setColumnValues(){
-		cnpColumn.setCellValueFactory(new Callback<CellDataFeatures<Patient, String>, ObservableValue<String>>() {
-			public ObservableValue<String> call(CellDataFeatures<Patient, String> p) {
-				return new ReadOnlyObjectWrapper<String>(p.getValue().getPatientRecord().getCNP());
-			}
-		});
-
-		firstNameColumn.setCellValueFactory(new Callback<CellDataFeatures<Patient, String>, ObservableValue<String>>() {
-			public ObservableValue<String> call(CellDataFeatures<Patient, String> p) {
-				return new ReadOnlyObjectWrapper<String>(p.getValue().getPatientRecord().getFirstName());
-			}
-		});
-
-		lastNameColumn.setCellValueFactory(new Callback<CellDataFeatures<Patient, String>, ObservableValue<String>>() {
-			public ObservableValue<String> call(CellDataFeatures<Patient, String> p) {
-				return new ReadOnlyObjectWrapper<String>(p.getValue().getPatientRecord().getLastName());
-			}
-		});
-
-		genderColumn.setCellValueFactory(new Callback<CellDataFeatures<Patient, String>, ObservableValue<String>>() {
-			public ObservableValue<String> call(CellDataFeatures<Patient, String> p) {
-				return new ReadOnlyObjectWrapper<String>(p.getValue().getPatientRecord().getSex().toString());
-			}
-		});
-
-		categoryColumn.setCellValueFactory(new Callback<CellDataFeatures<Patient, String>, ObservableValue<String>>() {
-			public ObservableValue<String> call(CellDataFeatures<Patient, String> p) {
-				return new ReadOnlyObjectWrapper<String>(p.getValue().getPatientRecord().getCategory().toString());
-			}
-		});
-
-		birthDateColumn.setCellValueFactory(new Callback<CellDataFeatures<Patient, String>, ObservableValue<String>>() {
-			public ObservableValue<String> call(CellDataFeatures<Patient, String> p) {
-				return new ReadOnlyObjectWrapper<String>(p.getValue().getPatientRecord().getBirthDateString());
-			}
-		});
-
-		deceaseDateColumn.setCellValueFactory(new Callback<CellDataFeatures<Patient, String>, ObservableValue<String>>() {
-			public ObservableValue<String> call(CellDataFeatures<Patient, String> p) {
-				return new ReadOnlyObjectWrapper<String>(p.getValue().getPatientRecord().getDeceaseDateString());
-			}
-		});
-
-		registrationDateColumn.setCellValueFactory(new Callback<CellDataFeatures<Patient, String>, ObservableValue<String>>() {
-			public ObservableValue<String> call(CellDataFeatures<Patient, String> p) {
-				return new ReadOnlyObjectWrapper<String>(p.getValue().getRegistrationRecord().getRegistrationDateString());
-			}
-		});
-
-		unregistrationDateColumn.setCellValueFactory(new Callback<CellDataFeatures<Patient, String>, ObservableValue<String>>() {
-			public ObservableValue<String> call(CellDataFeatures<Patient, String> p) {
-				return new ReadOnlyObjectWrapper<String>(p.getValue().getRegistrationRecord().getUnregistrationDateString());
-			}
-		});
-
-		countyColumn.setCellValueFactory(new Callback<CellDataFeatures<Patient, String>, ObservableValue<String>>() {
-			public ObservableValue<String> call(CellDataFeatures<Patient, String> p) {
-				return new ReadOnlyObjectWrapper<String>(p.getValue().getPatientRecord().getAddress().getCounty());
-			}
-		});
-
-		cityColumn.setCellValueFactory(new Callback<CellDataFeatures<Patient, String>, ObservableValue<String>>() {
-			public ObservableValue<String> call(CellDataFeatures<Patient, String> p) {
-				return new ReadOnlyObjectWrapper<String>(p.getValue().getPatientRecord().getAddress().getCity());
-			}
-		});
-
-		streetColumn.setCellValueFactory(new Callback<CellDataFeatures<Patient, String>, ObservableValue<String>>() {
-			public ObservableValue<String> call(CellDataFeatures<Patient, String> p) {
-				return new ReadOnlyObjectWrapper<String>(p.getValue().getPatientRecord().getAddress().getStreet());
-			}
-		});
-
 	}
 }
